75c8ced3864b925aaa10d5d9085d05b07c79d216
Mohammed Aljohani <msmjohani@hotmail.com>
2020-03-29 11:29:05 +0300

    create demo Curfew

---
+++ ConsoleApp2/ConsoleApp2/City.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    public class City
+    {
+        public int cityId { get; set; }
+        public string cityName { get; set; }
+        public static List<City> getAllCityHigherCurfew()
+        {
+
+            List<City> listStudents = new List<City>
+        {
+            new City
+            {
+                cityId = 1,
+                cityName = "Riyadh",
+            },
+                  new City
+            {
+                cityId = 2,
+                cityName = "Medina",
+            },
+                        new City
+            {
+                cityId = 3,
+                cityName = "Mecca",
+            },
+
+        };
+
+
+
+
+
+            return listStudents;
+
+
+
+
+        }
+        public static List<City> getAllCityLowerCurfew()
+        {
+
+            List<City> listStudents = new List<City>
+        {
+            new City
+            {
+                cityId = 4,
+                cityName = "Jeddah",
+            },
+                  new City
+            {
+                cityId = 5,
+                cityName = "Tabuk",
+            },
+                        new City
+            {
+                cityId = 6,
+                cityName = "Dammam",
+            },
+
+        };
+
+
+
+
+
+            return listStudents;
+
+
+
+
+        }
+
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Curfew in Saudi Arabia");
+            Console.WriteLine("**********************************************************************************");
+
+            List<City> list=  City.getAllCityHigherCurfew();
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.Write(" [ " +list[i].cityName + " ] ");
+            }
+
+
+            List<City> listlower = City.getAllCityLowerCurfew();
+            for (int i = 0; i < listlower.Count; i++)
+            {
+                Console.Write(" [ " + listlower[i].cityName + " ] ");
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("**********************************************************************************");
+            Console.WriteLine("Please Type City From Top ");
+            string cityName = Console.ReadLine();
+            var checklistHigher = list.Where(xx => xx.cityName == cityName).ToList();
+            var checklistlower = listlower.Where(xx => xx.cityName == cityName).ToList();
+            TimeSpan start = TimeSpan.Parse("15:00"); // 3:00 PM
+            TimeSpan end = TimeSpan.Parse("06:00");   // 6:00 AM
+            TimeSpan now = DateTime.Now.TimeOfDay;
+            Console.WriteLine("");
+            Console.WriteLine("**********************************************************************************");
+
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+            if (checklistHigher.Count >0)
+            {
+                Console.WriteLine(cityName + " The Curfew Begin from 3:00 pm to 6:00 am ");
+                if (now >= start  || now  <=end)
+
+                {
+                    Console.WriteLine("The Time Now : " + DateTime.Now.ToString("t") + " under curfew ");
+                }
+                else
+                {
+                    Console.WriteLine("The Time Now : " + DateTime.Now.ToString("t") + "  no curfew ");
+                }
+            }
+            else if (checklistlower.Count >0)
+            {
+                Console.WriteLine(cityName + " The Curfew Begin from 7:00 pm to 3:00 am ");
+                if ((now < start) && (now > end))
+
+                {
+                    Console.WriteLine("The Time Now : " + DateTime.Now.ToString("t") + "  no curfew ");
+                }
+                else
+                {
+                    Console.WriteLine("The Time Now : " + DateTime.Now.ToString("t") + " under curfew ");
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("sorry " + cityName + " Not found ");
+
+            }
+          
+
+        }
+    }
+}
